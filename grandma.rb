c72c0ef4f91cf8cf521b176b3df84d7780815630
thakaj <taulant.hakaj@scranton.edu>
2021-07-27 14:51:22 -0400

    commit tree 4b46117d76a4342cc9dceef6d8da846c2954a828

--- grandma.rb
+++ grandma.rb
@@ -11,3 +11,16 @@
 
 # However if you say 'I LOVE YOU GRANDMA!', she should respond with
 # 'I LOVE YOU TOO PUMPKIN!'
+
+
+def speak_to_grandma(input)
+    if input == "I LOVE YOU GRANDMA!"
+        return "I LOVE YOU TOO PUMPKIN!"
+    end
+    if input != phrase.uppercase()
+        return "HUH?! SPEAK UP, SONNY!"
+    end
+    else
+        return "NO, NOT SINCE 1938!"
+    end
+end
